637d582ced11dc0fe5b14e1728c4dff2643b71b8
Chevesade Ornano <chevesade.ornano@utp.ac.pa>
2026-03-25 16:41:13 -0500

    Update: se agragan imagenes para baner en el hero creadas con IA, se corrigen colores de botones y enlaces.

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,3 +85,17 @@ document.addEventListener("DOMContentLoaded", function() {
   }, 100);
 
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const slides = document.querySelectorAll(".hero-slide");
+  let current = 0;
+
+  if (slides.length > 0) {
+    setInterval(() => {
+      slides[current].classList.remove("active");
+      current = (current + 1) % slides.length;
+      slides[current].classList.add("active");
+    }, 4500); // cambia cada 4.5 segundos
+  }
+});
